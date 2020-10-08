89ae149d8695eda41543249a77bf7b002a7cbb90
Shivam Patel <shivampatel251097@gmail.com>
2020-10-08 17:36:16 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersService } from './servers/servers.service';
+
+const appRoutes: Routes = [
+{ path: '', component: HomeComponent },
+{ path: 'users', component: UsersComponent, children: [
+  { path: ':id/:name', component: UserComponent }
+] },
+{ path: 'servers', component: ServersComponent, children: [
+  { path: ':id/edit', component: EditServerComponent },
+  { path: ':id', component: ServerComponent }
+] }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ServersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
